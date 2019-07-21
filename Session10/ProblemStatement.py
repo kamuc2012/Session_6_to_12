db8e4ba4c3cb6026e0ebbd57db2dc1caea3a7573
Kamlesh Chetnani <kamlesh@saleshandy.com>
2019-07-21 18:29:07 +0530

    Added .ipynb files and screenshots

--- Session10/ProblemStatement.py
+++ Session10/ProblemStatement.py
@@ -30,47 +30,52 @@ df = pd.DataFrame({'From_To': ['LoNDon_paris', 'MAdrid_miLAN', 'londON_StockhOlm
 print(df)
 print()
 print(df.info())
-print("="*50)
+print("\n", "="*50, "\n", sep="")
 
 '''
 1. Some values in the the FlightNumber column are missing. These numbers are meant to increase by 10 with each row so 10055 and 10075 need to be put in place. Fill in these missing numbers and make the column an integer column (instead of a float column).
 '''
+print("Some values in the the FlightNumber column are missing. These numbers are meant to increase by 10 with each row so 10055 and 10075 need to be put in place. Fill in these missing numbers and make the column an integer column (instead of a float column).\n")
 print(df["FlightNumber"])
 print()
 df["FlightNumber"] = df["FlightNumber"].interpolate().astype(int)
 print(df["FlightNumber"])
-print("="*50)
+print("\n", "="*50, "\n", sep="")
 
 '''
 2. The From_To column would be better as two separate columns! Split each string on the underscore delimiter _ to give a new temporary DataFrame with the correct values. Assign the correct column names to this temporary DataFrame.
 '''
+print("The From_To column would be better as two separate columns! Split each string on the underscore delimiter _ to give a new temporary DataFrame with the correct values. Assign the correct column names to this temporary DataFrame.\n")
 new_columns = df["From_To"].str.split("_", n=1, expand=True)
 df["From"] = new_columns[0]
 df["To"] = new_columns[1]
 print(df)
-print("="*50)
+print("\n", "="*50, "\n", sep="")
 
 '''
 3. Notice how the capitalisation of the city names is all mixed up in this temporary DataFrame. Standardise the strings so that only the first letter is uppercase (e.g. "londON" should become "London".)
 '''
+print("""Notice how the capitalisation of the city names is all mixed up in this temporary DataFrame. Standardise the strings so that only the first letter is uppercase (e.g. "londON" should become "London".)\n""")
 df["From_To"] = df["From_To"].str.capitalize()
 df["From"] = df["From"].str.capitalize()
 df["To"] = df["To"].str.capitalize()
 print(df)
-print("="*50)
+print("\n", "="*50, "\n", sep="")
 
 '''
 4. Delete the From_To column from df and attach the temporary DataFrame from the previous questions.
 '''
+print("Delete the From_To column from df and attach the temporary DataFrame from the previous questions.\n")
 df.drop("From_To", axis=1, inplace=True)
 print(df)
-print("="*50)
+print("\n", "="*50, "\n", sep="")
 
 '''
 5. In the RecentDelays column, the values have been entered into the DataFrame as a list. We would like each first value in its own column, each second value in its own column, and so on. If there isn't an Nth value, the value should be NaN. 
 Expand the Series of lists into a DataFrame named delays, rename the columns delay_1, delay_2, etc. and replace the unwanted RecentDelays column in df with delays.
 '''
+print("Expand the Series of lists into a DataFrame named delays, rename the columns delay_1, delay_2, etc. and replace the unwanted RecentDelays column in df with delays.\n")
 df = df.assign(**pd.DataFrame(df["RecentDelays"].tolist()).add_prefix("delay_"))
 df.drop("RecentDelays", axis=1, inplace=True)
 print(df)
-print("="*50)
+print("\n", "="*50, "\n", sep="")
